c995f07953dfed380e7cdbaf6ae9f157a8965e04
terpyPy <66324329+terpyPy@users.noreply.github.com>
2023-04-06 14:42:54 -0400

    Update gameSettings.py
    reduced bloat of class by adding more efficient data structures.

--- gameSettings.py
+++ gameSettings.py
@@ -9,27 +9,43 @@
 #               I have left some patterns in the naming convention and access methods
 #               in this project making copy/pasted stolen code easy to parse and find.
 #
+import pygame
+
 class Settings:
     # store the game settings
-    def __init__(self, screen:tuple=(0,0), isGlobal:str=False) -> object:
+    def __init__(self, N:int,screen:tuple=(0,0), isGlobal:str=False) -> object:
         # screen settings
-        self.screen_width, self.screen_hight = screen
-        
+        self.screen_width, self.screen_height = screen
+        self.N = N
+        self.animation_FPS = 5
         if isGlobal == True:
             if screen == (0,0):
                 print('used default screen size')
             else:
-                print(f'screen_width :{self.screen_width}\n screen_hight: {self.screen_hight}')
+                print(f'screen_width :{self.screen_width}\n screen_hight: {self.screen_height}')
         else:
-            print(f'no screen settings used for _{isGlobal}_')
+            print(f'not screen settings for isGlobal. \nused {self.screen_width}:{self.screen_height}')
         self.bg_color = (40,40,40)
         # game driver settings
         self.isMulticolor = False
         self.isPause = False
-        self.modes = ['run', 'draw', 'nGame']
+        self.modes = ['run', 'draw', 'nGame',"AI"]
         self.mode = 'start'
-        self.onColor = (0,114,160)
-        self.offColor = (250,250,250)
-        self.winColor = (0, 25, 0)
-        self.simColor = (10, 93, 30)
+        self.menuKeys = {pygame.K_RIGHT: 'run',
+                    pygame.K_DOWN: 'draw',
+                    pygame.K_UP: 'nGame',
+                    pygame.K_LEFT: 'start',
+                    }
+        self.pauseKeys = {pygame.K_a: self.isPause,
+                            pygame.K_RETURN: (-2, False),}
+        self.onColor = [0,114,160]
+        self.offColor = [50,50,50]
+        self.winColor = [0, 25, 0]
+        self.simColor = [10, 93, 30]
         self.DEFAULT_SIZE_IMG = (60,60)
+        
+    def get_grid_padding(self) -> tuple:
+        return int(60)
+    
+    def get_temp_path(self) -> str:
+        return r"D:\2021code\NumpyAnimation\images\temp.bmp"
